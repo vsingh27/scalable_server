124fd201a149536be672d6fed4f55eec3df5c314
Rizwan <rizwan_ahmed21@hotmail.com>
2016-02-10 18:27:38 -0800

    some prototypes, not finished yet

--- select_server.h
+++ select_server.h
@@ -9,9 +9,10 @@
 #include <strings.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
+//end my misery 
 #define SERVER_PORT 8005 //default port
 #define BUFFER 1024 
+#define MAX_CLIENTS 200
 
 #define STATS_FILE "./select_serv_stats.csv"
 
@@ -22,9 +23,17 @@ typedef struct
 	int port; /* client's port number */
 	unsigned long requests; /* total requests from client */
 	unsigned long sent_data; /* total data sent from client */
-}
+} ClntStats;
 
+ClntStats clnt_stats[MAX_CLIENTS];
 
+static int clnt_pos = 0;
+
+void kill_server(int sig);
+void accept_connect(int fd);
+int create_socket();
+int set_socket_reuse(int* socket);
+int bindAddyToSocket(int* socket, int* port);
 
 
 #endif
