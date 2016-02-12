fa243edae5b78e4f6f480ba930d41dee12928d99
Rizwan Ahmed <rizwan_ahmed21@hotmail.com>
2016-02-12 03:31:42 -0800

    added some stuff using select call
    not done yet :joy: :joy: :joy:

--- select_server.cpp
+++ select_server.cpp
@@ -1,6 +1,175 @@
 #include "select_server.h"
 
+//globals
+struct timeval start, end;
+
 int main(int argc, char *argv[])
 {
 
+}
+
+void* run_server(int servPort)
+{
+
+	gettimeofday (&start, NULL);
+	
+    int i, maxi, nready, arg, t;
+    int listen_sd, new_sd, sockfd, maxfd, client[FD_SETSIZE];
+    struct sockaddr_in server, client_addr;
+    char buf[BUFLEN];
+    ssize_t n;
+    fd_set rset, allset;
+    socklen_t client_len;
+    int port = servPort;
+
+    if (servPort == 0) {
+
+        port = SERVER_PORT; // Use the default port
+
+    } else {
+        port = servPort; // Use the defined port
+    }
+    //printf("%d", FD_SETSIZE);
+
+    // Create a stream socket
+    if ((listen_sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        SystemFatal("Cannot Create Socket!");
+
+    // set SO_REUSEADDR so port can be reused immediately after exit, i.e., after CTRL-c
+    arg = 1;
+    if (setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof (arg)) == -1)
+        SystemFatal("setsockopt");
+
+    // Bind an address to the socket
+    bzero((char *) &server, sizeof (struct sockaddr_in));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
+
+    if (bind(listen_sd, (struct sockaddr *) &server, sizeof (server)) == -1)
+        SystemFatal("bind error");
+
+    // Listen for connections
+    // queue up to LISTENQ connect requests
+    listen(listen_sd, MAX_CONNECTIONS);
+
+    maxfd = listen_sd; // initialize
+    maxi = -1; // index into client[] array
+
+    for (i = 0; i < FD_SETSIZE; i++)
+        client[i] = -1; // -1 indicates available entry
+    FD_ZERO(&allset);
+    FD_SET(listen_sd, &allset);
+	
+    while (TRUE) {
+        rset = allset; // structure assignment
+        nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
+
+        if (FD_ISSET(listen_sd, &rset)){ // new client connection
+            client_len = sizeof (client_addr);
+            if ((new_sd = accept(listen_sd, (struct sockaddr *) &client_addr, &client_len)) == -1)
+                SystemFatal("accept error");
+
+            //printf(" Remote Address:  %s\n", inet_ntoa(client_addr.sin_addr));
+
+            for (i = 0; i < FD_SETSIZE; i++)
+                if (client[i] < 0) {
+                    client[i] = new_sd; // save descriptor
+                    break;
+                }
+            if (i == FD_SETSIZE) {
+                printf("Too many clients\n");
+                exit(1);
+            }
+
+            FD_SET(new_sd, &allset); // add new descriptor to set
+            if (new_sd > maxfd)
+                maxfd = new_sd; // for select
+
+            if (i > maxi)
+                maxi = i; // new max index in client[] array
+
+
+			
+			
+			// Get new client stats
+			//char * ip_address = inet_ntoa(client_addr.sin_addr);
+			//printf("CONNECTED TO: %s\n",ip_address);
+			//if((cstat = get_client_stats(ip_address)) == NULL)
+				SystemFatal("get_client_stats");
+			//cstat->total_conn++;
+			//cstat->conn++;
+
+            if (--nready <= 0)
+                continue; // no more readable descriptors
+        }
+
+        for (i = 0; i <= maxi; i++) // check all clients for data
+        {
+        	
+        	
+            if ((sockfd = client[i]) < 0)
+                continue;
+
+            if (FD_ISSET(sockfd, &rset)) {
+                n = read(sockfd, buf, BUFLEN);
+                if (n == BUFLEN) {
+                    write(sockfd, buf, BUFLEN); // echo to client
+                    //printf("(sockfd: %d)Sending: %s\n", sockfd, buf);
+					
+					// Get client's sockaddr_in
+					struct sockaddr addr;
+					socklen_t size = sizeof(struct sockaddr);
+					if((getpeername(sockfd, &addr, &size)) == -1){
+						SystemFatal("getpeername");
+					}
+
+					struct sockaddr_in * sin;
+					if(addr.sa_family == AF_INET){
+						sin = (struct sockaddr_in *)&addr;
+					}
+					else
+						SystemFatal("not ip4");
+					
+					// Get client stats
+					char * ip_address = inet_ntoa(sin->sin_addr);
+					if((cstat = get_client_stats(ip_address)) == NULL)
+						SystemFatal("get_client_stats");
+					cstat->msg++;
+				   	cstat->total_msg++;
+				   	cstat->data += n;
+					cstat->total_data += n;
+									
+                }
+
+                if (n == 0) // connection closed by client
+                {
+                    //printf("(sockfd: %d) Remote Address:  %s closed connection\n", sockfd, inet_ntoa(client_addr.sin_addr));
+                    close(sockfd);
+                    FD_CLR(sockfd, &allset);
+                    client[i] = -1;
+                    t++;
+                }
+                if (n == -1) {
+                    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                        ;
+                    } else {
+                        perror("read fail");
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+static void SystemFatal(const char* message)
+{
+	perror(message);
+	exit(EXIT_FAILURE);
+}
+
+void kill_server(int sig)
+{
+	exit(0);
 }
--- select_server.h
+++ select_server.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <strings.h>
 #include <arpa/inet.h>
@@ -12,7 +14,7 @@
 //It is estimated that a human is never more than 10 feet away from a spider—ever. 
 #define SERVER_PORT 8005 //default port
 #define BUFFER 1024 
-#define MAX_CLIENTS 200
+#define MAX_CLIENTS 700
 
 #define STATS_FILE "./select_serv_stats.csv"
 
@@ -31,6 +33,7 @@ static int clnt_pos = 0;
 
 void kill_server(int sig);
 void accept_connect(int *socket);
+void* run_server(int servPort);
 int create_socket();
 int set_socket_reuse(int* socket);
 int bindAddyToSocket(int* socket, int* port);
