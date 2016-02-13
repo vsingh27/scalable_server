cec4a498595580fc2c190e22221c8bce57d3ac2c
Rizwan Ahmed <RizAhmed@users.noreply.github.com>
2016-02-12 19:28:18 -0800

    more includes for select server

--- select_server.h
+++ select_server.h
@@ -2,8 +2,11 @@
 #define SELECT_SERVER
 
 #include <stdio.h>
+#include <string.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <sys/time.h>
 #include <netinet/in.h>
 #include <pthread.h>
@@ -11,9 +14,12 @@
 #include <strings.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-//It is estimated that a human is never more than 10 feet away from a spider—ever. 
+#include <errno.h>
+#include <assert.h>
+#include <sys/wait.h>
+//It is estimated that a human is never more than 10 feet away from a spiderï¿½ever.
 #define SERVER_PORT 8005 //default port
-#define BUFFER 1024 
+#define BUFFER 1024
 #define MAX_CLIENTS 700
 
 #define STATS_FILE "./select_serv_stats.csv"
