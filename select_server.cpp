bf45d2a62a021005318f06b262b3b1f953dfda51
Rizwan Ahmed <RizAhmed@users.noreply.github.com>
2016-02-12 19:26:37 -0800

    fixed some compiler errors
    need to fix void* conversion error

--- select_server.cpp
+++ select_server.cpp
@@ -1,5 +1,6 @@
 #include "select_server.h"
 
+//friday 1925 revision
 //globals
 struct timeval start, end;
 pthread_t tm; //main thread
@@ -10,6 +11,7 @@ int fd_server;
 
 int main(int argc, char *argv[])
 {
+	//(void)signal(SIGINT, kill_server);
 	setbuf(stdout, NULL);
 	run_server(SERVER_PORT);
 
@@ -18,66 +20,63 @@ int main(int argc, char *argv[])
 int client_exists(char * checkAddress)
 {
 	int c;
-    for (c = 0; c < srvrStats_len; c++) {
-        if (print_debug == 2)
-            printf("Comparing [%s] with [%s]\n", checkAddress, srvr_stats[c].address);
+	for (c = 0; c < srvrStats_len; c++) {
 
-        if ((strcmp(checkAddress, srvr_stats[c].address)) == 0) {
-            return c;
-        }
-    }
-    return -1;
+		if ((strcmp(checkAddress, srvr_stats[c].address)) == 0) {
+			return c;
+		}
+	}
+	return -1;
 }
 
 ClntStats * get_stats(char * ipAddress)
 {
-    int c;
-    if ((c = client_exists(ipAddress)) != -1) {
-        return &srvr_stats[c];
+	int c;
+	if ((c = client_exists(ipAddress)) != -1) {
+		return &srvr_stats[c];
 
-    } else {
+	} else {
 
-        if ((srvr_stats = realloc((void *) srvr_stats, sizeof (ClntStats) * (srvrStats_len + 1))) != NULL) {
-            srvrStats_len++;
+		if ((srvr_stats = realloc(srvr_stats, sizeof (ClntStats) * (srvrStats_len + 1))) != NULL) {
+			srvrStats_len++;
 
-            //Initialize server_stats
-            memset(srvr_stats[srvrStats_len - 1].address, 0, 32);
-            // Copy address to struct
-            strcpy(srvr_stats[srvrStats_len - 1].address, address);
-            srvr_stats[srvrStats_len - 1].total_connect = 0;
-            srvr_stats[srvrStats_len - 1].curr_connect = 0;
-            srvr_stats[srvrStats_len - 1].total_message = 0;
-            srvr_stats[srvrStats_len - 1].message = 0;
-            srvr_stats[srvrStats_len - 1].total_data = 0;
-            srvr_stats[srvrStats_len - 1].curr_data = 0;
+			//Initialize server_stats
+			memset(srvr_stats[srvrStats_len - 1].address, 0, 32);
+			// Copy address to struct
+			strcpy(srvr_stats[srvrStats_len - 1].address, ipAddress);
+			srvr_stats[srvrStats_len - 1].total_connect = 0;
+			srvr_stats[srvrStats_len - 1].curr_connect = 0;
+			srvr_stats[srvrStats_len - 1].total_message = 0;
+			srvr_stats[srvrStats_len - 1].message = 0;
+			srvr_stats[srvrStats_len - 1].total_data = 0;
+			srvr_stats[srvrStats_len - 1].curr_data = 0;
 
-            return &srvr_stats[srvrStats_len - 1];
-        }
-    }
-    return NULL;
-	
+			return &srvr_stats[srvrStats_len - 1];
+		}
+	}
+	return NULL;
+
 }
 
-void* live_stats
+void* live_stats()
 {
-    int c = 0;
-    int p1 = 0, p2 = 0;
-    long p3 = 0, p4 = 0, p5 = 0, p6 = 0, p7 = 0, p8 = 0;
+	int c = 0;
+	int p1 = 0, p2 = 0;
+	long p3 = 0, p4 = 0, p5 = 0, p6 = 0, p7 = 0, p8 = 0;
+	int t1 = 0, t2 = 0;
+	long t3 = 0, t4 = 0, t5 = 0, t6 = 0, t7 = 0, t8 = 0;
 
-    int t1 = 0, t2 = 0;
-    long t3 = 0, t4 = 0, t5 = 0, t6 = 0, t7 = 0, t8 = 0;
+	char line[108];
+	for (c = 0; c < 107; c++)
+	line[c] = '-';
+	line[c] = '\0';
 
-    char line[108];
-    for (c = 0; c < 107; c++)
-        line[c] = '-';
-    line[c] = '\0';
+	while (1) {
 
-    while (1) {
-
-        gettimeofday(&end, NULL);
-        float total_time = (float) (end.tv_sec - start.tv_sec) + ((float) (end.tv_usec - start.tv_usec) / 1000000);
-        printf("\nElapsed Time: %.3fs\n", total_time);
-        printf("%-14s%-14s%-14s%-14s%-14s%-14s%-14s\n",\
+		gettimeofday(&end, NULL);
+		float total_time = (float) (end.tv_sec - start.tv_sec) + ((float) (end.tv_usec - start.tv_usec) / 1000000);
+		printf("\nElapsed Time: %.3fs\n", total_time);
+		printf("%-14s%-14s%-14s%-14s%-14s%-14s%-14s\n",\
 		"Clients",\
 		"Total Connections",\
 		"Active Connections",\
@@ -85,25 +84,22 @@ void* live_stats
 		"Msg/s",\
 		"Received Bytes",\
 		"Byte/s");
-        printf("%s\n", line);
+		printf("%s\n", line);
 
-        if (print_debug == 2)
-            printf("server_stat_len:%d\n", srvrStats_len);
+		for (c = 0; c < srvrStats_len; c++) {
+			// Pre stats
+			t1 += p1 = srvr_stats[c].total_connect;
+			t2 += p2 = srvr_stats[c].curr_connect;
 
-        for (c = 0; c < srvrStats_len; c++) {
-            // Pre stats
-            t1 += p1 = srvr_stats[c].total_connct;
-            t2 += p2 = srvr_stats[c].curr_connect;
+			t4 += p4 = srvr_stats[c].message;
+			t3 += p3 = srvr_stats[c].total_message;
 
-            t4 += p4 = srvr_stats[c].message;
-            t3 += p3 = srvr_stats[c].total_message;
-
-            t6 += p6 = srvr_stats[c].curr_data;
-            t5 += p5 = srvr_stats[c].total_data;
+			t6 += p6 = srvr_stats[c].curr_data;
+			t5 += p5 = srvr_stats[c].total_data;
 
 
-            // Print stats
-            printf("%-14s%-14d%-14d%-14ld%-14ld%-14ld%-14ld\n",\
+			// Print stats
+			printf("%-14s%-14d%-14d%-14ld%-14ld%-14ld%-14ld\n",\
 			srvr_stats[c].address,\
 			p1,\
 			p2,\
@@ -112,13 +108,13 @@ void* live_stats
 			p5,\
 			p6);
 
-            // Post stats
-            srver_stats[c].message = 0;
-            srver_stats[c].curr_data = 0;
-        }
+			// Post stats
+			srvr_stats[c].message = 0;
+			srvr_stats[c].curr_data = 0;
+		}
 
-        // Print totals
-        printf("%-14s%-14d%-14d%-14ld%-14ld%-14ld%-14ld\n\n",\
+		// Print totals
+		printf("%-14s%-14d%-14d%-14ld%-14ld%-14ld%-14ld\n\n",\
 		"Total",\
 		t1,\
 		t2,\
@@ -127,168 +123,167 @@ void* live_stats
 		t5,\
 		t6);
 
-        // Reset totals
-        t1 = 0;
-        t2 = 0;
-        t3 = 0;
-        t4 = 0;
-        t5 = 0;
-        t6 = 0;
-        sleep(1);
-    }
+		// Reset totals
+		t1 = 0;
+		t2 = 0;
+		t3 = 0;
+		t4 = 0;
+		t5 = 0;
+		t6 = 0;
+		sleep(1);
+	}
 
 }
 
 void* run_server(int serv_port)
 {
 	gettimeofday (&start, NULL);
-	
+
 	//start recording live stats
 	pthread_create(&tm, NULL, &live_stats, NULL);
-	
-    int i, maxi, nready, arg, t;
-    int listen_sd, new_sd, sockfd, maxfd, client[FD_SETSIZE];
-    struct sockaddr_in server, client_addr;
-    char buf[BUFLEN];
-    ssize_t n;
-    fd_set rset, allset;
-    socklen_t client_len;
-    int port = serv_port; // Use the default port
-	
+
+	int i, maxi, nready, arg, t;
+	int listen_sd, new_sd, sockfd, maxfd, client[FD_SETSIZE];
+	struct sockaddr_in server, client_addr;
+	char buf[BUFFER];
+	ssize_t n;
+	fd_set rset, allset;
+	socklen_t client_len;
+	int port = serv_port; // Use the default port
+
 	//create the socket
 	if ((listen_sd = socket(AF_INET, SOCK_STREAM,0))== -1)
-		SystemFatal("Cannot create socket!");
-	
+	SystemFatal("Cannot create socket!");
+
 	// set SO_REUSEADDR so port can be resused imemediately after exit, i.e., after CTRL-c
-    arg = 1;
-    if (setsockopt (listen_sd, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof(arg)) == -1)
-		SystemFatal("setsockopt");
-		
+	arg = 1;
+	if (setsockopt (listen_sd, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof(arg)) == -1)
+	SystemFatal("setsockopt");
+
 	// Bind an address to the socket
-    bzero((char *) &server, sizeof (struct sockaddr_in));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
+	bzero((char *) &server, sizeof (struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
 
 	if (bind(listen_sd, (struct sockaddr *) &server, sizeof (server)) == -1)
-        SystemFatal("bind error");
-	
-	
+	SystemFatal("bind error");
+
+
 	// Listen for connections
-    // queue up to LISTENQ connect requests
-    listen(listen_sd, MAX_CONNECTIONS);
+	// queue up to LISTENQ connect requests
+	listen(listen_sd, MAX_CLIENTS);
 
-    maxfd = listen_sd; // initialize
-    maxi = -1; // index into client[] array
+	maxfd = listen_sd; // initialize
+	maxi = -1; // index into client[] array
 
-    for (i = 0; i < FD_SETSIZE; i++)
-        client[i] = -1; // -1 indicates available entry
-    FD_ZERO(&allset);
-    FD_SET(listen_sd, &allset
-	
-	ClntStats * clnt_stats;
-	
-	while (TRUE) {
-        rset = allset; // structure assignment
-        nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
+	for (i = 0; i < FD_SETSIZE; i++)
+	client[i] = -1; // -1 indicates available entry
+	FD_ZERO(&allset);
+	FD_SET(listen_sd, &allset);
 
-        if (FD_ISSET(listen_sd, &rset)){ // new client connection
-            client_len = sizeof (client_addr);
-            if ((new_sd = accept(listen_sd, (struct sockaddr *) &client_addr, &client_len)) == -1)
-                SystemFatal("accept error");
+		ClntStats * clnt_stats;
 
-            for (i = 0; i < FD_SETSIZE; i++)
-                if (client[i] < 0) {
-                    client[i] = new_sd; // save descriptor
-                    break;
-                }
-            if (i == FD_SETSIZE) {
-                printf("Too many clients\n");
-                exit(1);
-            }
+		while (1) {
+			rset = allset; // structure assignment
+			nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
 
-            FD_SET(new_sd, &allset); // add new descriptor to set
-            if (new_sd > maxfd)
-                maxfd = new_sd; // for select
+			if (FD_ISSET(listen_sd, &rset)){ // new client connection
+				client_len = sizeof (client_addr);
+				if ((new_sd = accept(listen_sd, (struct sockaddr *) &client_addr, &client_len)) == -1)
+				SystemFatal("accept error");
 
-            if (i > maxi)
-                maxi = i; // new max index in client[] array
-			
-			// Get new client stats
-			char * ip_address = inet_ntoa(client_addr.sin_addr);
-			//printf("CONNECTED TO: %s\n",ip_address);
-			if((clnt_stats = get_stats(ip_address)) == NULL)
+				for (i = 0; i < FD_SETSIZE; i++)
+				if (client[i] < 0) {
+					client[i] = new_sd; // save descriptor
+					break;
+				}
+				if (i == FD_SETSIZE) {
+					printf("Too many clients\n");
+					exit(1);
+				}
+
+				FD_SET(new_sd, &allset); // add new descriptor to set
+				if (new_sd > maxfd)
+				maxfd = new_sd; // for select
+
+				if (i > maxi)
+				maxi = i; // new max index in client[] array
+
+				// Get new client stats
+				char * ip_address = inet_ntoa(client_addr.sin_addr);
+				//printf("CONNECTED TO: %s\n",ip_address);
+				if((clnt_stats = get_stats(ip_address)) == NULL)
 				SystemFatal("get_stats");
-			clnt_stats->total_connect++;
-			clnt_stats->curr_connect++;
+				clnt_stats->total_connect++;
+				clnt_stats->curr_connect++;
 
-            if (--nready <= 0)
-                continue; // no more readable descriptors
-        }
+				if (--nready <= 0)
+				continue; // no more readable descriptors
+			}
 
-        for (i = 0; i <= maxi; i++) // check all clients for data
-        {
-            if ((sockfd = client[i]) < 0)
-                continue;
+			for (i = 0; i <= maxi; i++) // check all clients for data
+			{
+				if ((sockfd = client[i]) < 0)
+				continue;
 
-            if (FD_ISSET(sockfd, &rset)) {
-                n = read(sockfd, buf, BUFLEN);
-                if (n == BUFLEN) {
-                    write(sockfd, buf, BUFLEN); // echo to client
-					// Get client's sockaddr_in
-					struct sockaddr addr;
-					socklen_t size = sizeof(struct sockaddr);
-					if((getpeername(sockfd, &addr, &size)) == -1){
-						SystemFatal("getpeername");
-					}
+				if (FD_ISSET(sockfd, &rset)) {
+					n = read(sockfd, buf, BUFFER);
+					if (n == BUFFER) {
+						write(sockfd, buf, BUFFER); // echo to client
+						// Get client's sockaddr_in
+						struct sockaddr addr;
+						socklen_t size = sizeof(struct sockaddr);
+						if((getpeername(sockfd, &addr, &size)) == -1){
+							SystemFatal("getpeername");
+						}
 
-					struct sockaddr_in * sin;
-					if(addr.sa_family == AF_INET){
-						sin = (struct sockaddr_in *)&addr;
-					}
-					else
+						struct sockaddr_in * sin;
+						if(addr.sa_family == AF_INET){
+							sin = (struct sockaddr_in *)&addr;
+						}
+						else
 						SystemFatal("not ip4");
-					
-					// Get client stats
-					char * ip_address = inet_ntoa(sin->sin_addr);
-					if((clnt_stats = get_stats(ip_address)) == NULL)
+
+						// Get client stats
+						char * ip_address = inet_ntoa(sin->sin_addr);
+						if((clnt_stats = get_stats(ip_address)) == NULL)
 						SystemFatal("get_client_stats");
-					clnt_stats->message++;
-				   	clnt_stats->total_message++;
-				   	clnt_stats->data += n;
-					clnt_stats->total_data += n;
-									
-                }
+						clnt_stats->message++;
+						clnt_stats->total_message++;
+						clnt_stats->curr_data += n;
+						clnt_stats->total_data += n;
 
-                if (n == 0) // connection closed by client
-                {
-                    //printf("(sockfd: %d) Remote Address:  %s closed connection\n", sockfd, inet_ntoa(client_addr.sin_addr));
-                    close(sockfd);
-                    FD_CLR(sockfd, &allset);
-                    client[i] = -1;
-                    t++;
-                }
-                if (n == -1) {
-                    if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                        ;
-                    } else {
-                        perror("read fail");
-                    }
-                }
-            }
-        }
-    }	
-}
+					}
 
-static void SystemFatal(const char* message)
-{
-	perror(message);
-	exit(EXIT_FAILURE);
-}
+					if (n == 0) // connection closed by client
+					{
+						//printf("(sockfd: %d) Remote Address:  %s closed connection\n", sockfd, inet_ntoa(client_addr.sin_addr));
+						close(sockfd);
+						FD_CLR(sockfd, &allset);
+						client[i] = -1;
+						t++;
+					}
+					if (n == -1) {
+						if (errno == EAGAIN || errno == EWOULDBLOCK) {
+						} else {
+							perror("read fail");
+						}
+					}
+				}
+			}
+		}
+	}
 
-void kill_server(int sig)
-{
-	pthread_kill(tm, 0);
-	close(fd_server);
-	exit(0);
-}
+	static void SystemFatal(const char* message)
+	{
+		perror(message);
+		exit(EXIT_FAILURE);
+	}
+
+	void kill_server(int sig)
+	{
+		pthread_kill(tm, 0);
+		close(fd_server);
+		exit(0);
+	}
